9ab6e5af0d6131f70349913b60ee37d494a1bed1
x-cold <lxstart@outlook.com>
2021-10-11 15:36:41 +0800

    fix: typo error

--- src/chatpter1/1.2 basic-types.ts
+++ src/chatpter1/1.2 basic-types.ts
@@ -8,8 +8,8 @@ let count: number = 100;
 let str: string = 'string string string';
 
 // String Literal
-type EmailLocaleIDs = "welcome_email" | "email_heading";
-type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+type EmailLocaleIDs = 'welcome_email' | 'email_heading';
+type FooterLocaleIDs = 'footer_title' | 'footer_sendoff';
  
 type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
 
